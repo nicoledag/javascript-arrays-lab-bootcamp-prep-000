ab62a1eb5b730ab47e2a11252bea0866c8214a80
Nicole D <nnardella@gmail.com>
2018-09-08 20:37:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your functions and code here
+
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendKitten(Ralph) {
+kittens.push(Ralph);
+return kittens
+}
+
+
+function destructivelyPrependKitten(Bob) {
+kittens.unshift(Bob);
+return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens
+}
+
+
+function appendKitten(Broom){
+  return [...kittens, Broom];
+}
